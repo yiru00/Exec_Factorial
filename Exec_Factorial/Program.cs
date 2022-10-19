0c3fc8d81eb1e36f22df2bd6cb2a8ab1e8f23af8
yiru <eva890706@gmail.com>
2022-10-19 09:33:55 +0800

    作業上傳

---
+++ Exec_Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exec_Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("請輸入一個數值: ");
+			string input=Console.ReadLine();
+			bool isInt=int.TryParse(input, out int num); // true => int num=Convert.Int32(input);
+			int start = 1;
+			if (isInt == false)
+			{
+				Console.WriteLine("錯誤，請輸入數字");
+				return;	
+			}
+			for (int i =1 ; i <=num; i++)
+			{
+				start=start *i;
+			}
+			Console.WriteLine($"{num}! = {start}");
+
+		}
+	}
+}
